f22dd31c2a159118ba6e96784dcd8349ec9f72b8
Javier Fernandes <javier.fernandes@gmail.com>
2018-06-20 22:14:36 -0300

    implementing script

--- add-metadata.sh
+++ add-metadata.sh
@@ -4,7 +4,10 @@ FOLDER="content/posts/"
 
 function create_metadata() {
   FILE=$1
-  (echo -e "---\ntitle: \"$FILE\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat ${FOLDER}$(basename $FILE)) > $FILE.mig
+  NAME=$(basename $FILE)
+  NAME_NO_EXT=$(echo $NAME | cut -f1 -d'.')
+  echo $NAME_NO_EXT
+  (echo -e "---\ntitle: \"$FILE\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat ${FOLDER}$($NAME)) > $NAME_NO_EXT.md
 }
 
 for f in ${FOLDER}*.md.md; do create_metadata "$f"; done
