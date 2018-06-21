48f1b06ef5c4a254350ff4e2ed6702fa7da7c36f
Javier Fernandes <javier.fernandes@gmail.com>
2018-06-20 22:17:40 -0300

    fixing script

--- add-metadata.sh
+++ add-metadata.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FOLDER="content/posts/"
+FOLDER="./content/posts/"
 
 function create_metadata() {
   FILE=$1
   NAME=$(basename $FILE)
   NAME_NO_EXT=$(echo $NAME | cut -f1 -d'.')
   echo $NAME_NO_EXT
-  (echo -e "---\ntitle: \"$FILE\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat ${FOLDER}$($NAME)) > $NAME_NO_EXT.md
+  echo (echo -e "---\ntitle: \"$FILE\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat ${FOLDER}$($FILE)) > $NAME_NO_EXT.md
 }
 
 for f in ${FOLDER}*.md.md; do create_metadata "$f"; done
