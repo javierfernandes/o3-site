861e8fc5444d3c5a2869f8c18500c443918c34c2
Javier Fernandes <javier.fernandes@gmail.com>
2018-06-20 22:31:22 -0300

    Fixing title to only include file name

--- add-metadata.sh
+++ add-metadata.sh
@@ -7,7 +7,7 @@ function create_metadata() {
   NAME=$(basename $FILE)
   NAME_NO_EXT=$(echo $NAME | cut -f1 -d'.')
   echo $NAME_NO_EXT
-  (echo -e "---\ntitle: \"$FILE\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat $FILE) > $FOLDER$NAME_NO_EXT.md
+  (echo -e "---\ntitle: \"$NAME_NO_EXT\"\ndate:  2018-06-20T19:27:10-03:00\n---\n\n"; cat $FILE) > $FOLDER$NAME_NO_EXT.md
 }
 
 for f in ${FOLDER}*.md.md; do create_metadata "$f"; done
